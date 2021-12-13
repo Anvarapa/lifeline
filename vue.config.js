6ee94269f71869830a4220019651b8296778c2ce
anvar <turdaliev.anvar11@gmail.com>
2021-12-13 21:03:35 +0300

    testing

--- vue.config.js
+++ vue.config.js
@@ -5,16 +5,16 @@ module.exports = {
   ,
 
 
-  chainWebpack: (config) => {
-    const svgRule = config.module.rule("svg");
-
-    svgRule.uses.clear();
-
-    svgRule
-      .use("babel-loader")
-      .loader("babel-loader")
-      .end()
-      .use("vue-svg-loader")
-      .loader("vue-svg-loader");
-  },
+  // chainWebpack: (config) => {
+  //   const svgRule = config.module.rule("svg");
+  //
+  //   svgRule.uses.clear();
+  //
+  //   svgRule
+  //     .use("babel-loader")
+  //     .loader("babel-loader")
+  //     .end()
+  //     .use("vue-svg-loader")
+  //     .loader("vue-svg-loader");
+  // },
 };
